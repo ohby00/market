7f29a3a4f5d51d308a4a707dcb8c413554386fee
already-cow <alreadycow_@naver.com>
2024-05-29 19:41:04 +0900

    [Modify] : workflow.yml

--- user-service/src/main/java/com/osio/userservice/config/LocalConfig.java
+++ user-service/src/main/java/com/osio/userservice/config/LocalConfig.java
@@ -1,27 +1,27 @@
-package com.osio.userservice.config;
-
-import lombok.Data;
-import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.stereotype.Component;
-
-@Data
-@RefreshScope       // Config.yml 파일 변경 시 변경된 내용을 actuator 통해 변경값을 갱신
-@Component
-public class LocalConfig {
-    // {application-profiles}.yml 에 정의한 내용을 해당 변수에 넣어줌
-    @Value("${spring.datasource.username}")
-    private String username;
-
-    @Value("${spring.datasource.password}")
-    private String password;
-
-    @Override
-    public String toString() {
-        return "LocalConfig{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
-}
+//package com.osio.userservice.config;
+//
+//import lombok.Data;
+//import lombok.Getter;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.stereotype.Component;
+//
+//@Data
+//@RefreshScope       // Config.yml 파일 변경 시 변경된 내용을 actuator 통해 변경값을 갱신
+//@Component
+//public class LocalConfig {
+//    // {application-profiles}.yml 에 정의한 내용을 해당 변수에 넣어줌
+//    @Value("${spring.datasource.username}")
+//    private String username;
+//
+//    @Value("${spring.datasource.password}")
+//    private String password;
+//
+//    @Override
+//    public String toString() {
+//        return "LocalConfig{" +
+//                "username='" + username + '\'' +
+//                ", password='" + password + '\'' +
+//                '}';
+//    }
+//}
